9080906c34e6b73c26e5de720c140342f05b329d
jinnyjinnyjinjin <kkam2ya@gmail.com>
2023-03-01 15:59:26 +0900

    first commit

---
+++ src/test/java/com/jinnyjinnyjinjin/redischatserver/RedisChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinnyjinnyjinjin.redischatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
