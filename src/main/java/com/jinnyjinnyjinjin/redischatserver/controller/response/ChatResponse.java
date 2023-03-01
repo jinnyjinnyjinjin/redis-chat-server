4eebf2bb07b6873701ef079feaa60b482cd978ff
jinnyjinnyjinjin <kkam2ya@gmail.com>
2023-03-01 17:32:16 +0900

    Add ChatResponse dto

---
+++ src/main/java/com/jinnyjinnyjinjin/redischatserver/controller/response/ChatResponse.java
@@ -0,0 +1,12 @@
+package com.jinnyjinnyjinjin.redischatserver.controller.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ChatResponse {
+    String content;
+}
