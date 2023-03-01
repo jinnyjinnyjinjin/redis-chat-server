25bda32ae5baddd062d19d0367ef520095034457
jinnyjinnyjinjin <kkam2ya@gmail.com>
2023-03-01 17:33:28 +0900

    Add serializable

--- src/main/java/com/jinnyjinnyjinjin/redischatserver/model/ChatRoom.java
+++ src/main/java/com/jinnyjinnyjinjin/redischatserver/model/ChatRoom.java
@@ -3,11 +3,16 @@ package com.jinnyjinnyjinjin.redischatserver.model;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.UUID;
 
 @Builder
 @Getter
-public class ChatRoom {
+public class ChatRoom implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 6494678977089006639L;
 
     private String roomId;
     private String name;
