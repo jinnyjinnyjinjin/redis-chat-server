199b1c6bdbf3dddfb7b9b81a6bdff6cc0c198b5a
jinnyjinnyjinjin <kkam2ya@gmail.com>
2023-03-01 17:33:14 +0900

    Add isEnter method to validate message type

--- src/main/java/com/jinnyjinnyjinjin/redischatserver/common/MessageType.java
+++ src/main/java/com/jinnyjinnyjinjin/redischatserver/common/MessageType.java
@@ -1,5 +1,9 @@
 package com.jinnyjinnyjinjin.redischatserver.common;
 
 public enum MessageType {
-    ENTER, QUIT, TALK
+    ENTER, QUIT, TALK;
+
+    public static boolean isEnter(MessageType type) {
+        return MessageType.ENTER == type;
+    }
 }
